bd84227b58df012134f528e8ba8c2cc1eeb8d9c5
Chavo Kim <treeboy0384@naver.com>
2021-11-02 09:30:06 +0900

    Feat: Added skeleton design for main

---
+++ android/app/src/main/kotlin/com/chavokim/bookfriendly_main/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chavokim.bookfriendly_main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '읽어양득'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedIdx = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Color(0xFF6F4A32),
+        backgroundColor: Color(0xFFFFFFFF),
+        title: Text(widget.title, style: TextStyle(color: Color(0xFF6F4A32))),
+        centerTitle: false,
+        elevation: 0,
+        shadowColor: null,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$selectedIdx',
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Color(0xFF6F4A32),
+        unselectedItemColor: Color(0xFF222222),
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "홈",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "검색",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box_outlined),
+            label: "글쓰기",
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: "소통",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: "내 책장",
+          ),
+        ],
+        currentIndex: selectedIdx,
+        onTap: (idx) => {
+          setState(() => {
+            selectedIdx = idx
+          })
+        },
+      ),
+    );
+  }
+}
